efd7f091a083a5508045ca800548f5d152c7feed
Malcolm Roberts <malcolm.i.w.roberts@gmail.com>
2014-11-26 16:58:30 +0100

    Update output of cl errors

--- clutils.c
+++ clutils.c
@@ -10,193 +10,193 @@ const char* clErrorString(const cl_int err)
 
   switch (err) {
   case CL_SUCCESS:
-    errString = "Success";
+    errString = "CL_SUCCESS";
     break;
             
   case CL_DEVICE_NOT_FOUND:
-    errString = "Device not found";
+    errString = "CL_DEVICE_NOT_FOUND";
     break;
             
   case CL_DEVICE_NOT_AVAILABLE:
-    errString = "Device not available";
+    errString = "CL_DEVICE_NOT_AVAILABLE";
     break;
             
   case CL_COMPILER_NOT_AVAILABLE:
-    errString = "Compiler not available";
+    errString = "CL_COMPILER_NOT_AVAILABLE";
     break;
             
   case CL_MEM_OBJECT_ALLOCATION_FAILURE:
-    errString = "Memory object allocation failure";
+    errString = "CL_MEM_OBJECT_ALLOCATION_FAILURE";
     break;
             
   case CL_OUT_OF_RESOURCES:
-    errString = "Out of resources";
+    errString = "CL_OUT_OF_RESOURCES";
     break;
             
   case CL_OUT_OF_HOST_MEMORY:
-    errString = "Out of host memory";
+    errString = "CL_OUT_OF_HOST_MEMORY";
     break;
             
   case CL_PROFILING_INFO_NOT_AVAILABLE:
-    errString = "Profiling info not available";
+    errString = "CL_PROFILING_INFO_NOT_AVAILABLE";
     break;
             
   case CL_MEM_COPY_OVERLAP:
-    errString = "Memory copy overlap";
+    errString = "CL_MEM_COPY_OVERLAP";
     break;
             
   case CL_IMAGE_FORMAT_MISMATCH:
-    errString = "Image format mismatch";
+    errString = "CL_IMAGE_FORMAT_MISMATCH";
     break;
             
   case CL_IMAGE_FORMAT_NOT_SUPPORTED:
-    errString = "Image format not supported";
+    errString = "CL_IMAGE_FORMAT_NOT_SUPPORTED";
     break;
             
   case CL_BUILD_PROGRAM_FAILURE:
-    errString = "Build program failure";
+    errString = "CL_BUILD_PROGRAM_FAILURE";
     break;
             
   case CL_MAP_FAILURE:
-    errString = "Map failure";
+    errString = "CL_MAP_FAILURE";
     break;
             
   case CL_INVALID_VALUE:
-    errString = "Invalid value";
+    errString = "CL_INVALID_VALUE";
     break;
             
   case CL_INVALID_DEVICE_TYPE:
-    errString = "Invalid device type";
+    errString = "CL_INVALID_DEVICE_TYP";
     break;
             
   case CL_INVALID_PLATFORM:
-    errString = "Invalid platform";
+    errString = "CL_INVALID_PLATFORM";
     break;
             
   case CL_INVALID_DEVICE:
-    errString = "Invalid device";
+    errString = "CL_INVALID_DEVICE";
     break;
             
   case CL_INVALID_CONTEXT:
-    errString = "Invalid context";
+    errString = "CL_INVALID_CONTEXT";
     break;
             
   case CL_INVALID_QUEUE_PROPERTIES:
-    errString = "Invalid queue properties";
+    errString = "CL_INVALID_QUEUE_PROPERTIES";
     break;
             
   case CL_INVALID_COMMAND_QUEUE:
-    errString = "Invalid command queue";
+    errString = "CL_INVALID_COMMAND_QUEUE";
     break;
             
   case CL_INVALID_HOST_PTR:
-    errString = "Invalid host pointer";
+    errString = "CL_INVALID_HOST_PTR";
     break;
             
   case CL_INVALID_MEM_OBJECT:
-    errString = "Invalid memory object";
+    errString = "CL_INVALID_MEM_OBJECT";
     break;
             
   case CL_INVALID_IMAGE_FORMAT_DESCRIPTOR:
-    errString = "Invalid image format descriptor";
+    errString = "CL_INVALID_IMAGE_FORMAT_DESCRIPTOR";
     break;
             
   case CL_INVALID_IMAGE_SIZE:
-    errString = "Invalid image size";
+    errString = "CL_INVALID_IMAGE_SIZE";
     break;
             
   case CL_INVALID_SAMPLER:
-    errString = "Invalid sampler";
+    errString = "CL_INVALID_SAMPLER";
     break;
             
   case CL_INVALID_BINARY:
-    errString = "Invalid binary";
+    errString = "CL_INVALID_BINARY";
     break;
             
   case CL_INVALID_BUILD_OPTIONS:
-    errString = "Invalid build options";
+    errString = "CL_INVALID_BUILD_OPTIONS";
     break;
             
   case CL_INVALID_PROGRAM:
-    errString = "Invalid program";
+    errString = "CL_INVALID_PROGRAM";
     break;
             
   case CL_INVALID_PROGRAM_EXECUTABLE:
-    errString = "Invalid program executable";
+    errString = "CL_INVALID_PROGRAM_EXECUTABLE";
     break;
             
   case CL_INVALID_KERNEL_NAME:
-    errString = "Invalid kernel name";
+    errString = "CL_INVALID_KERNEL_NAME";
     break;
             
   case CL_INVALID_KERNEL_DEFINITION:
-    errString = "Invalid kernel definition";
+    errString = "CL_INVALID_KERNEL_DEFINITION";
     break;
             
   case CL_INVALID_KERNEL:
-    errString = "Invalid kernel";
+    errString = "CL_INVALID_KERNEL";
     break;
             
   case CL_INVALID_ARG_INDEX:
-    errString = "Invalid argument index";
+    errString = "CL_INVALID_ARG_INDEX";
     break;
             
   case CL_INVALID_ARG_VALUE:
-    errString = "Invalid argument value";
+    errString = "CL_INVALID_ARG_VALUE";
     break;
             
   case CL_INVALID_ARG_SIZE:
-    errString = "Invalid argument size";
+    errString = "CL_INVALID_ARG_SIZE";
     break;
             
   case CL_INVALID_KERNEL_ARGS:
-    errString = "Invalid kernel arguments";
+    errString = "CL_INVALID_KERNEL_ARGS";
     break;
             
   case CL_INVALID_WORK_DIMENSION:
-    errString = "Invalid work dimension";
+    errString = "CL_INVALID_WORK_DIMENSION";
     break;
             
   case CL_INVALID_WORK_GROUP_SIZE:
-    errString = "Invalid work group size";
+    errString = "CL_INVALID_WORK_GROUP_SIZE";
     break;
             
   case CL_INVALID_WORK_ITEM_SIZE:
-    errString = "invalid work item size";
+    errString = "CL_INVALID_WORK_ITEM_SIZE";
     break;
             
   case CL_INVALID_GLOBAL_OFFSET:
-    errString = "Invalid global offset";
+    errString = "CL_INVALID_GLOBAL_OFFSET";
     break;
             
   case CL_INVALID_EVENT_WAIT_LIST:
-    errString = "Invalid event wait list";
+    errString = "CL_INVALID_EVENT_WAIT_LIST";
     break;
             
   case CL_INVALID_EVENT:
-    errString = "Invalid event";
+    errString = "CL_INVALID_EVENT";
     break;
             
   case CL_INVALID_OPERATION:
-    errString = "Invalid operation";
+    errString = "CL_INVALID_OPERATION";
     break;
             
   case CL_INVALID_GL_OBJECT:
-    errString = "Invalid OpenGL object";
+    errString = "CL_INVALID_GL_OBJECT";
     break;
             
   case CL_INVALID_BUFFER_SIZE:
-    errString = "Invalid buffer size";
+    errString = "CL_INVALID_BUFFER_SIZE";
     break;
             
   case CL_INVALID_MIP_LEVEL:
-    errString = "Invalid MIP level";
+    errString = "CL_INVALID_MIP_LEVEL";
     break;
 
   default:
     errString = "UNKOWN OPENCL ERROR";
   }
-    
+  
   return errString;
 }
 
