6f7ab83095eba47244425adcbec92760a68b7ed7
Malcolm Roberts <malcolmiwroberts@gmail.com>
2016-01-17 11:01:37 +0100

    Clean up tests/fft3

--- tests/fft3.cpp
+++ tests/fft3.cpp
@@ -15,20 +15,6 @@
 
 using namespace std;
 
-template<class T>
-void init(T *X, unsigned int nx, unsigned int ny, unsigned int nz)
-{
-  for(unsigned int i = 0; i < nx; ++i) {
-    for(unsigned int j = 0; j < ny; ++j) {
-      for(unsigned int k = 0; k < nz; ++k) {
-	int pos = i * nz * ny + j * nz + k;
-	X[2 * pos] = i;
-	X[2 * pos + 1] = j + k * k;
-      }
-    }
-  }
-}
-
 int main(int argc, char *argv[]) {
   int platnum = 0;
   int devnum = 0;
@@ -137,6 +123,7 @@ const unsigned int ny, const unsigned int nz)\n	\
   X[2 * pos] = i;				\
   X[2 * pos + 1] = j + k * k;			\
 }\n";
+  size_t global_wsize[] = {nx, ny, nz};
   cl_program initprog = create_program(init_source, ctx);
   build_program(initprog, device);
   cl_kernel initkernel = create_kernel(initprog, "init"); 
@@ -144,55 +131,43 @@ const unsigned int ny, const unsigned int nz)\n	\
   set_kernel_arg(initkernel, 1, sizeof(unsigned int), &ny);
   set_kernel_arg(initkernel, 2, sizeof(unsigned int), &nz);
 
-  cout << "Allocating " 
-	    << fft.ncomplex() 
-	    << " doubles." << endl;
-  double *X = new double[2 * fft.ncomplex()];
-  double *FX = new double[2 * fft.ncomplex()];
+  cout << "Allocating " << 2 * nx * ny * nz << " doubles." << endl;
+  double *X = new double[2 * nx * ny * nz];
+  double *FX = new double[2 * nx * ny * nz];
 
-  cl_event clv_init = clCreateUserEvent(ctx, NULL);
-  cl_event clv_toram = clCreateUserEvent(ctx, NULL);
-  cl_event clv_forward = clCreateUserEvent(ctx, NULL);
-  cl_event clv_backward = clCreateUserEvent(ctx, NULL);
   if(N == 0) { // Transform forwards and back, outputting the buffer.
     tolerance *= 1.0 + log((double) max(max(nx, ny), nz));
     cout << "Tolerance: " << tolerance << endl;
 
-    init(X, nx, ny, nz);
-    
     cout << "\nInput:" << endl;
+    clEnqueueNDRangeKernel(queue, initkernel, 3, NULL,  global_wsize, NULL,
+			   0, 0, 0);
+    clFinish(queue);
+    clEnqueueReadBuffer(queue, inbuf, CL_TRUE, 0,
+			sizeof(double) * 2 * nx * ny * nz, X, 0, 0, 0);
+    clFinish(queue);
     if(nx * ny * nz <= maxout) 
       show3C(X, nx, ny, nz);
     else 
       cout << X[0] << endl;
 
-    //fft.ram_to_cbuf(X, &inbuf, 0, NULL, &clv_init);
-    size_t global_wsize[] = {nx, ny, nz};
-    clEnqueueNDRangeKernel(queue,
-			   initkernel,
-			   3, // cl_uint work_dim,
-			   NULL, // global_work_offset,
-			   global_wsize, // global_work_size, 
-			   NULL, // size_t *local_work_size, 
-			   0, NULL, &clv_init);
-
-    fft.forward(&inbuf, inplace ? NULL : &outbuf, 1, &clv_init, &clv_forward);
-    fft.cbuf_to_ram(FX, inplace ? &inbuf :&outbuf, 1, &clv_forward, &clv_toram);
-    clWaitForEvents(1, &clv_toram);
-    
     cout << "\nTransformed:" << endl;
+    fft.forward(&inbuf, inplace ? NULL : &outbuf, 0, 0, 0);
+    clFinish(queue);
+    clEnqueueReadBuffer(queue, inplace ? inbuf : outbuf, CL_TRUE, 0,
+			sizeof(double) * 2 * nx * ny * nz, FX, 0, 0, 0);
+    clFinish(queue);
     if(nx * ny * nz <= maxout) 
       show3C(FX, nx, ny, nz);
     else 
       cout << X[0] << endl;
 
-    fft.backward(inplace ? &inbuf : &outbuf,
-		 inplace ? NULL : &outbuf, 
-		 1, &clv_forward, &clv_backward);
-    fft.cbuf_to_ram(X, &inbuf, 1, &clv_backward, &clv_toram);
-    clWaitForEvents(1, &clv_toram);
-
     cout << "\nTransformed back:" << endl;
+    fft.backward(inplace ? &inbuf : &outbuf, inplace ? NULL : &outbuf, 0, 0, 0);
+    clFinish(queue);
+    clEnqueueReadBuffer(queue, inplace ? inbuf : outbuf, CL_TRUE, 0,
+			sizeof(double) * 2 * nx * ny * nz, X, 0, 0, 0);
+    clFinish(queue);
     if(nx * ny * nz <= maxout) 
       show3C(X, nx, ny, nz);
     else 
@@ -200,8 +175,14 @@ const unsigned int ny, const unsigned int nz)\n	\
     
     // Compute the round-trip error.
     {
-      double *X0 = new double[2 * fft.ncomplex()];
-      init(X0, nx, ny, nz);
+      double *X0 = new double[2 * nx * ny * nz];
+      clEnqueueNDRangeKernel(queue, initkernel, 3, NULL,  global_wsize, NULL,
+			     0, 0, 0);
+      clFinish(queue);
+      clEnqueueReadBuffer(queue, inbuf, CL_TRUE, 0,
+			  sizeof(double) * 2 * nx * ny * nz, X0, 0, 0, 0);
+      clFinish(queue);
+
       double L2error = 0.0;
       double maxerror = 0.0;
       for(unsigned int i = 0; i < fft.ncomplex(); ++i) {
@@ -234,10 +215,16 @@ const unsigned int ny, const unsigned int nz)\n	\
       fftwpp::fft3d Forward(-1, f);
       fftwpp::fft3d Backward(1, f);
       double *df = (double *)f();
-      init(df, nx, ny, nz);
-      //show3C(df, nx, ny, nz);
+
+      clEnqueueNDRangeKernel(queue, initkernel, 3, NULL,  global_wsize, NULL,
+			     0, 0, 0);
+      clFinish(queue);
+      clEnqueueReadBuffer(queue, inbuf, CL_TRUE, 0,
+			  sizeof(double) * 2 * nx * ny * nz, df, 0, 0, 0);
+      clFinish(queue);
+
       Forward.fft(f);
-      //show3C(df, nx, ny, nz);
+
 
       double L2error = 0.0;
       double maxerror = 0.0;
@@ -269,19 +256,11 @@ const unsigned int ny, const unsigned int nz)\n	\
   
     cl_ulong time_start, time_end;
     for(unsigned int i = 0; i < N; i++) {
-      //init(X, nx, ny, nz);
-      //fft.ram_to_cbuf(X, &inbuf, 0, NULL, &clv_init);
-      size_t global_wsize[] = {nx, ny, nz};
-      clEnqueueNDRangeKernel(queue,
-			     initkernel,
-			     3, // cl_uint work_dim,
-			     NULL, // global_work_offset,
-			     global_wsize, // global_work_size, 
-			     NULL, // size_t *local_work_size, 
-			     0, NULL, &clv_init);
-
-      fft.forward(&inbuf, inplace ? NULL : &outbuf, 
-		  1, &clv_init, &clv_forward);
+      cl_event clv_forward;
+      clEnqueueNDRangeKernel(queue, initkernel, 3, NULL,  global_wsize, NULL,
+			     0, 0, 0);
+      clFinish(queue);
+      fft.forward(&inbuf, inplace ? NULL : &outbuf,  0, 0, &clv_forward);
       clWaitForEvents(1, &clv_forward);
 
       clGetEventProfilingInfo(clv_forward,
